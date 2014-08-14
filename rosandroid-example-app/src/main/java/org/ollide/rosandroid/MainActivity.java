af3a15f7297736b0526c5611e020321cd5d31c40
Oliver Degener <o.degener@monday-consulting.com>
2014-08-14 15:55:51 +0200

    removes android menu boilerplate code

--- rosandroid-example-app/src/main/java/org/ollide/rosandroid/MainActivity.java
+++ rosandroid-example-app/src/main/java/org/ollide/rosandroid/MainActivity.java
@@ -1,15 +1,12 @@
 package org.ollide.rosandroid;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import org.ros.address.InetAddressFactory;
 import org.ros.android.RosActivity;
 import org.ros.node.NodeConfiguration;
 import org.ros.node.NodeMain;
 import org.ros.node.NodeMainExecutor;
-
 
 public class MainActivity extends RosActivity {
 
@@ -21,26 +18,6 @@ public class MainActivity extends RosActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
